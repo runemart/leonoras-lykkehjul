eab009f69c3b4dc4f9f5b6ef8376e678a901d77b
Rune M. Andersen <rune.andersen@gmail.com>
2023-09-10 16:58:59 +0200

    New launcher icon. Added konfetti. Tweaked speed settings.

--- app/src/main/java/com/runemartin/leonorasapp/MainActivity.kt
+++ app/src/main/java/com/runemartin/leonorasapp/MainActivity.kt
@@ -1,33 +1,60 @@
 package com.runemartin.leonorasapp
 
 import android.os.Bundle
-import android.view.View
 import android.view.animation.DecelerateInterpolator
-import android.widget.Button
+import android.viewbinding.library.activity.viewBinding
 import androidx.appcompat.app.AppCompatActivity
+import com.runemartin.leonorasapp.databinding.ActivityMainBinding
+import nl.dionsegijn.konfetti.core.Party
+import nl.dionsegijn.konfetti.core.emitter.Emitter
+import nl.dionsegijn.konfetti.core.models.Size
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewBinding: ActivityMainBinding by viewBinding()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button = findViewById<Button>(R.id.knapp)
-        val wheel = findViewById<View>(R.id.wheel)
-
-        button.setOnClickListener {
-            wheel.rotation = wheel.rotation % 360
-            wheel.animate()
-                .rotation(wheel.rotation - 90f)
-                .setDuration(500).withEndAction {
-                    wheel.animate()
-                        .rotation((360*5 + Math.random() * 360 * 10).toFloat())
-                        .setInterpolator(DecelerateInterpolator(3f))
-                        .setDuration(10000)
-                        .start()
-                }
+        viewBinding.apply {
+            button.setOnClickListener {
+                wheel.rotation = wheel.rotation % 360
+                wheel.animate()
+                    .rotation(wheel.rotation - 90f)
+                    .setDuration(500)
+                    .withEndAction { spinWheel() }
+            }
         }
+    }
 
+    private fun spinWheel() {
+        viewBinding.wheel.animate()
+            .rotation((360 * 5 + Math.random() * 360 * 10).toFloat())
+            .setInterpolator(DecelerateInterpolator())
+            .setDuration(7000)
+            .withEndAction { konfetti() }
+            .start()
+    }
+
+    private fun konfetti() {
+        val colorInt = viewBinding.wheel.colorForDegree(viewBinding.wheel.rotation)
+        try {
+            viewBinding.konfetti.start(
+                Party(
+                    colors = listOf(colorInt),
+                    emitter = Emitter(duration = 200).max(100),
+                    speed = 10f,
+                    maxSpeed = 30f,
+                    damping = 0.95f,
+                    angle = 270,
+                    spread = 90,
+                    size = listOf(Size.LARGE),
+                )
+            )
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 
 }
--- app/src/main/java/com/runemartin/leonorasapp/Wheel.kt
+++ app/src/main/java/com/runemartin/leonorasapp/Wheel.kt
@@ -2,12 +2,12 @@ package com.runemartin.leonorasapp
 
 import android.content.Context
 import android.graphics.Canvas
-import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
 import androidx.core.graphics.toColorInt
+import kotlin.math.floor
 
 class Wheel @JvmOverloads constructor(
     context: Context,
@@ -15,19 +15,24 @@ class Wheel @JvmOverloads constructor(
     defStyle: Int = 0,
 ) : View(context, attrs, defStyle) {
 
-
     private val paint = Paint().apply {
         style = Paint.Style.FILL
     }
 
     private val colors = listOf(
-        "#CC0088".toColorInt(),
-        "#06D6A0".toColorInt(),
-        "#1B9AAA".toColorInt(),
-        "#EF476F".toColorInt(),
-        "#FFC43D".toColorInt(),
-        "#666666".toColorInt(),
+        "#fed102".toColorInt(),
+        "#ffa504".toColorInt(),
+        "#ff4f44".toColorInt(),
+        "#bc5fd4".toColorInt(),
+        "#1198f4".toColorInt(),
+        "#48c521".toColorInt(),
     )
+
+    fun colorForDegree(degree: Float): Int {
+        val adjustedDegree = (degree + 90) % 360
+        val colorNr = floor(adjustedDegree / degreePrSection).toInt()
+        return colors[colors.size - colorNr - 1]
+    }
 
     private val degreePrSection = 360f / colors.size
 
